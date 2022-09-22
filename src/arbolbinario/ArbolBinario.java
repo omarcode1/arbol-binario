ecfdb505991c955f5d1162612307c8f21e3077cf
Jesus Mamamni <jesus765uriarte@gmail.com>
2022-09-22 19:39:14 -0400

    Adicion de menu- practica 1

--- src/arbolbinario/ArbolBinario.java
+++ src/arbolbinario/ArbolBinario.java
@@ -5,6 +5,8 @@
  */
 package arbolbinario;
 
+import java.util.Scanner;
+
 /**
  *
  * @author omarl
@@ -15,37 +17,88 @@ public class ArbolBinario {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        ArbolEnteros arbol = new ArbolEnteros();
-        arbol.insertar(1);
-        arbol.insertar(2);
-        arbol.insertar(3);
-        arbol.insertar(4);
-        arbol.insertar(0);
-        System.out.println("Recorriendo inorden:");
-        arbol.inorden();
-        System.out.println("Recorriendo postorden:");
-        arbol.postorden();
-        System.out.println("Recorriendo preorden:");
-        arbol.preorden();
-        System.out.println(arbol.existe(1)); // true
-        System.out.println(arbol.existe(7)); // false
-        System.out.println(arbol.existe(0)); // true
+        Scanner sc = new Scanner(System.in);
+        int opcionMenu = 0;
+        int opcion2 = 0;
+        do {
 
-        ArbolCadenas arbolCadenas = new ArbolCadenas();
-        arbolCadenas.insertar("Luis");
-        arbolCadenas.insertar("Chris");
-        arbolCadenas.insertar("Zelda");
-        arbolCadenas.insertar("Cuphead");
-        arbolCadenas.insertar("Leon");
-        System.out.println("Recorriendo inorden:");
-        arbolCadenas.inorden();
-        System.out.println("Recorriendo postorden:");
-        arbolCadenas.postorden();
-        System.out.println("Recorriendo preorden:");
-        arbolCadenas.preorden();
-        System.out.println(arbolCadenas.existe("Luis")); // true
-        System.out.println(arbolCadenas.existe("Claire")); // false
-        System.out.println(arbolCadenas.existe("Zelda")); // true
+            System.out.println("\n\n");
+            System.out.println(" |                 MENÚ                         |");
+            System.out.println(" |-----------------------|-----------------------|");
+            System.out.println(" | 1. Recorriendo inorden | 6.Recorriendo preorden (texto)|");
+            System.out.println(" | 2. Recorriendo postorden        | 9.                   |");
+            System.out.println(" | 3. Recorriendo preorden         | 10.                  |");
+            System.out.println(" | 4. Recorriendo inorden(texto)   | 20. Salir            |");
+            System.out.println(" | 5. Recorriendo postorden(texto) | 20. Salir            |");
+            System.out.println(" | 20. Salir       |");
+            System.out.println(" |-----------------------|-----------------------|");
+            System.out.print(" Elija una Opción: ");
+            opcionMenu = sc.nextInt();
+            ArbolEnteros arbol = new ArbolEnteros();
+            arbol.insertar(1);
+            arbol.insertar(2);
+            arbol.insertar(3);
+            arbol.insertar(4);
+            arbol.insertar(0);
+
+            ArbolCadenas arbolCadenas = new ArbolCadenas();
+            arbolCadenas.insertar("Luis");
+            arbolCadenas.insertar("Chris");
+            arbolCadenas.insertar("Zelda");
+            arbolCadenas.insertar("Cuphead");
+            arbolCadenas.insertar("Leon");
+            switch (opcionMenu) {
+                case 1:
+                    System.out.println("Recorriendo inorden:");
+                    arbol.inorden();
+                    System.out.println(arbol.existe(1)); // true
+                    System.out.println(arbol.existe(7)); // false
+                    System.out.println(arbol.existe(0)); // true
+                    break;
+                case 2:
+                    System.out.println("Recorriendo postorden:");
+                    arbol.postorden();
+                    System.out.println(arbol.existe(1)); // true
+                    System.out.println(arbol.existe(7)); // false
+                    System.out.println(arbol.existe(0)); // true
+                    break;
+                case 3:
+                    System.out.println("Recorriendo preorden:");
+                    arbol.preorden();
+                    System.out.println(arbol.existe(1)); // true
+                    System.out.println(arbol.existe(7)); // false
+                    System.out.println(arbol.existe(0)); // true
+                    break;
+
+                case 4:
+
+                    System.out.println("Recorriendo inorden:");
+                    arbolCadenas.inorden();
+                    System.out.println(arbolCadenas.existe("Luis")); // true
+                    System.out.println(arbolCadenas.existe("Claire")); // false
+                    System.out.println(arbolCadenas.existe("Zelda")); // true
+                    break;
+                case 5:
+                    System.out.println("Recorriendo postorden:");
+                    arbolCadenas.postorden();
+                    System.out.println(arbolCadenas.existe("Luis")); // true
+                    System.out.println(arbolCadenas.existe("Claire")); // false
+                    System.out.println(arbolCadenas.existe("Zelda")); // true
+                    break;
+                case 6:
+                    System.out.println("Recorriendo preorden:");
+                    arbolCadenas.preorden();
+                    System.out.println(arbolCadenas.existe("Luis")); // true
+                    System.out.println(arbolCadenas.existe("Claire")); // false
+                    System.out.println(arbolCadenas.existe("Zelda")); // true
+                    break;
+
+                default:
+                    System.out.println("\n\n Opción no válida! \n");
+
+                    break;
+            }
+        } while (opcionMenu != 20);
+
     }
-
 }
